c4923179d4b2c5194d7266ce966c27e23cfc1f79
Andrew Bean <andrew.bean@aya.yale.edu>
2024-03-19 15:20:47 +0000

    cleaning up code and initial runs

--- baseline_script.py
+++ baseline_script.py
@@ -15,30 +15,32 @@ import os
 
 ## Options to change regularly
 
-with open('../../home/blac0817/tokens.json') as f:
+with open('../../home/andrew/tokens.json') as f:
     tokens = json.load(f)
 hf_token = tokens['hugging_face']
 
 
-models = { 'Mistral 8x7B': 'mistralai/Mixtral-8x7B-v0.1',
-           'Meditron 7B': 'epfl-llm/meditron-7b',
-           'Llama 2 7B':  'meta-llama/Llama-2-7b-chat-hf',
-           'Gemma 7B': 'google/gemma-7b-it',
+models = { #'Mistral 8x7B': {'name':'mistralai/Mixtral-8x7B-v0.1','context':32000},
+           #'Meditron 7B': {'name':'epfl-llm/meditron-7b','context':4096},
+           #'Llama 2 7B':  {'name':'meta-llama/Llama-2-7b-chat-hf','context':4096},
+           'Llama 13 7B':  {'name':'meta-llama/Llama-2-13b-chat-hf','context':4096},
+           #'Gemma 7B': {'name':'google/gemma-7b-it','context':8192}
           }
 
-model_name = models['Gemma 7B']
+model = 'Meditron 7B'
 
-cache_dir = '../../data/blac0817/huggingface'
-os.putenv("HF_HOME", cache_dir)
+#cache_dir = '../../data/blac0817/huggingface'
+#os.putenv("HF_HOME", cache_dir)
 #questions_path = "../../data/blac0817/data/master_questions.csv"
 questions_path = 'medqa'
 
-batch_size = 10
+batch_size = 4
 question_limit = 100000  # for testing
 randomize_choices = False
 
-out_folder = "../../data/blac0817/human-learning-strategies/responses"
-filename = f"{model_name.split('/')[-1]}_medqa.json"
+out_folder = "../../code/human-learning-strategies/responses"
+file_suffix = '_medqa.json'
+
 
 ## Data structure for outputs
 
@@ -86,11 +88,25 @@ def make_batch(questions, answers, indices, shuffle, batch_size=64):
         batches.append({'questions':questions[i : i + batch_size], 'answers':answers[i : i + batch_size], 'index':indices[i : i + batch_size], 'shuffles':shuffle[i : i + batch_size]})
     return batches
 
+def format_prompt(row, choices):
+    answer_choices = [row[c] for c in choices]
+    keys = list(range(len(choices)))
+    if randomize_choices:
+        random.shuffle(keys)
+
+    options = ' '.join([f'{choices[i]}) {answer_choices[keys[i]]}' for i in range(len(keys))])
+    text = f"Answer the following multiple choice question by giving the most appropriate response. The answer should be one of {choices}. Question: {row['prompt']} {options}"
+    answer = choices[keys.index(choices.index(row['Answer']))]
+    return pd.Series([text, answer, keys],index=['question','answer', 'shuffle'])
+        
+
 def load_medqa(randomize_choices=False):
     ### Loads the medqa dataset and parses it into the desired format
     med_qa = load_dataset('bigbio/med_qa','med_qa_en_source',trust_remote_code=True,split='train')
     df = med_qa.to_pandas()
     choices = ['A','B','C','D','E']
+
+    df = df.rename(columns={'question':'prompt','answer_idx':'Answer'})
 
     random.seed(0)
 
@@ -98,16 +114,7 @@ def load_medqa(randomize_choices=False):
     choices_df.columns = choices
     df = pd.concat([df,choices_df],axis=1)
 
-    def format_prompt(row, choices):
-        answer_choices = [row[c] for c in choices]
-        keys = list(range(len(choices)))
-        if randomize_choices:
-            random.shuffle(keys)
-        text = f"Answer the following multiple choice question by giving the most appropriate response. The answer should be one of [A, B, C, D, E]. Question: {row['question']} A) {answer_choices[keys[0]]} B) {answer_choices[keys[1]]} C) {answer_choices[keys[2]]} D) {answer_choices[keys[3]]} E) {answer_choices[keys[4]]}"
-        answer = choices[keys.index(choices.index(row['answer_idx']))]
-        return pd.Series([text, answer, keys],index=['question','answer', 'shuffle'])
-    
-    shuffled_df = df[['question', 'A', 'B', 'C', 'D', 'E','answer_idx']].apply(lambda x: format_prompt(x,choices),axis=1)
+    shuffled_df = df[['prompt', 'A', 'B', 'C', 'D', 'E','Answer']].apply(lambda x: format_prompt(x,choices),axis=1)
     questions = shuffled_df['question'].values
     correct_answers = shuffled_df['answer'].values
     shuffle = shuffled_df['shuffle'].values
@@ -120,22 +127,13 @@ def load_medmcqa(randomize_choices=False):
     df = medmcqa.to_pandas()
     choices = ['A','B','C','D']
 
-    df = df.rename(columns={'opa': 'A', 'opb': 'B', 'opc': 'C', 'opd': 'D'})
+    df = df.rename(columns={'question':'prompt','opa': 'A', 'opb': 'B', 'opc': 'C', 'opd': 'D','answer':'Answer'})
 
     df['answer'] = df['cop'].apply(lambda x: choices[x])
 
     random.seed(0)
 
-    def format_prompt(row, choices):
-        answer_choices = [row[c] for c in choices]
-        keys = list(range(len(choices)))
-        if randomize_choices:
-            random.shuffle(keys)
-        text = f"Answer the following multiple choice question by giving the most appropriate response. The answer should be one of [A, B, C, D]. Question: {row['question']} A) {answer_choices[keys[0]]} B) {answer_choices[keys[1]]} C) {answer_choices[keys[2]]} D) {answer_choices[keys[3]]}"
-        answer = choices[keys.index(choices.index(row['answer']))]
-        return pd.Series([text, answer, keys],index=['question','answer', 'shuffle'])
-    
-    shuffled_df = df[['question', 'A', 'B', 'C', 'D','answer']].apply(lambda x: format_prompt(x,choices),axis=1)
+    shuffled_df = df[['prompt', 'A', 'B', 'C', 'D','Answer']].apply(lambda x: format_prompt(x,choices),axis=1)
     questions = shuffled_df['question'].values
     correct_answers = shuffled_df['answer'].values
     shuffle = shuffled_df['shuffle'].values
@@ -149,7 +147,7 @@ def load_questions(questions_path, randomize_choices=False):
     if questions_path == 'medqa':
         questions, correct_answers, q_idx, shuffle, choices = load_medqa(randomize_choices)
 
-    elif questions_path 'medmcqa':
+    elif questions_path == 'medmcqa':
         questions, correct_answers, q_idx, shuffle, choices = load_medqa(randomize_choices)
 
     else:
@@ -159,15 +157,6 @@ def load_questions(questions_path, randomize_choices=False):
         df = pd.read_csv(questions_path, ).set_index('Unnamed: 0')
         random.seed(0)
 
-        def format_prompt(row, choices):
-            answer_choices = [row[c] for c in choices]
-            keys = list(range(len(choices)))
-            if randomize_choices:
-                random.shuffle(keys)
-            text = f"Answer the following multiple choice question by giving the most appropriate response. The answer should be one of [A, B, C, D, E]. Question: {row['prompt']} A) {answer_choices[keys[0]]} B) {answer_choices[keys[1]]} C) {answer_choices[keys[2]]} D) {answer_choices[keys[3]]} E) {answer_choices[keys[4]]}"
-            answer = choices[keys.index(choices.index(row['Answer']))]
-            return pd.Series([text, answer, keys],index=['question','answer', 'shuffle'])
-        
         shuffled_df = df[['prompt', 'A', 'B', 'C', 'D', 'E','Answer']].apply(lambda x: format_prompt(x,choices),axis=1)
         questions = shuffled_df['question'].values
         correct_answers = shuffled_df['answer'].values
@@ -184,56 +173,63 @@ def load_questions(questions_path, randomize_choices=False):
 
 ## Loading the model 
 
-# Set device to GPU if available
-device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+for model_pretty_name in models.keys():
 
-tokenizer = AutoTokenizer.from_pretrained(
-    model_name, padding_side="left", trust_remote_code=True, token=hf_token
-)
-tokenizer.pad_token = tokenizer.eos_token
+    model_name = models[model_pretty_name]['name']
 
-bnb_config = BitsAndBytesConfig(
-    load_in_8bit=True,
-    #bnb_4bit_compute_dtyp=torch.bfloat16,
-)
+    filename = f"{model_name.split('/')[-1]}{file_suffix}"
 
-# faster on single gpu if the model can fit, change "auto" to "cuda:0"
-model = AutoModelForCausalLM.from_pretrained(
-    model_name,
-    token=hf_token,
-    #torch_dtype=torch.bfloat16,
-    device_map="auto",
-    cache_dir=cache_dir,
-    # use_flash_attention_2=True,
-    trust_remote_code=True,
-    quantization_config=bnb_config,
-)
+    # Set device to GPU if available
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
 
-# not yet available on python 3.12
-# model = torch.compile(model, mode="reduce-overhead", fullgraph=True)
+    tokenizer = AutoTokenizer.from_pretrained(
+        model_name, padding_side="left", trust_remote_code=True, token=hf_token
+    )
+    tokenizer.pad_token = tokenizer.eos_token
 
-questions, correct_answers, q_idx, shuffles, choices = load_questions(questions_path, randomize_choices=randomize_choices)
-questions, correct_answers, q_idx, shuffles = questions[:question_limit], correct_answers[:question_limit], q_idx[:question_limit], shuffles[:question_limit]
+    #bnb_config = BitsAndBytesConfig(
+        #load_in_8bit=True,
+        #bnb_4bit_compute_dtyp=torch.bfloat16,
+    #)
 
-max_length = np.max([len(q) for q in questions])
-qas = QAs([])
-choice_tokens = tokenizer(choices, add_special_tokens=False, return_tensors="pt", max_length=1, padding="max_length", truncation=True).input_ids.tolist()
+    # faster on single gpu if the model can fit, change "auto" to "cuda:0"
+    model = AutoModelForCausalLM.from_pretrained(
+        model_name,
+        token=hf_token,
+        torch_dtype=torch.bfloat16,
+        device_map="cuda:0",
+        # use_flash_attention_2=True,
+        trust_remote_code=True,
+        #quantization_config=bnb_config,
+    )
+
+    # not yet available on python 3.12
+    # model = torch.compile(model, mode="reduce-overhead", fullgraph=True)
+
+    questions, correct_answers, q_idx, shuffles, choices = load_questions(questions_path, randomize_choices=randomize_choices)
+    questions, correct_answers, q_idx, shuffles = questions[:question_limit], correct_answers[:question_limit], q_idx[:question_limit], shuffles[:question_limit]
+
+    model_context = models[model_pretty_name]['context']
+    max_length = np.max([len(q) for q in questions])
+    max_length = np.min([max_length,model_context])
+    qas = QAs([])
+    choice_tokens = tokenizer(choices, add_special_tokens=False, return_tensors="pt", max_length=1, padding="max_length", truncation=True).input_ids.tolist()
 
 
-for batch in tqdm(make_batch(questions, correct_answers, q_idx, shuffles, batch_size=batch_size)):
-    
-    response = rank_multichoice(batch['questions'], max_length, choice_tokens)
-
-    rs = [
-            QA(q, a, int(idx), s, choices[r.argmax()], [choices[i] for i in r.argsort().flip(0)[:3]], r.tolist())
-            for q, a, idx, s, r in zip(
-                batch['questions'], batch['answers'], batch['index'], batch['shuffles'], response
-            )
-        ]
-
-    qas.questions.extend(rs)
-
+    for batch in tqdm(make_batch(questions, correct_answers, q_idx, shuffles, batch_size=batch_size)):
         
-folder = Path(out_folder)
-folder.mkdir(exist_ok=True)
-(folder / filename).write_text(json.dumps(qas.to_dict(), indent=4))
+        response = rank_multichoice(batch['questions'], max_length, choice_tokens)
+
+        rs = [
+                QA(q, a, int(idx), s, choices[r.argmax()], [choices[i] for i in r.argsort().flip(0)[:3]], r.tolist())
+                for q, a, idx, s, r in zip(
+                    batch['questions'], batch['answers'], batch['index'], batch['shuffles'], response
+                )
+            ]
+
+        qas.questions.extend(rs)
+
+            
+    folder = Path(out_folder)
+    folder.mkdir(exist_ok=True)
+    (folder / filename).write_text(json.dumps(qas.to_dict(), indent=4))
