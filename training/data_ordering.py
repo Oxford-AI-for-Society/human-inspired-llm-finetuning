18d58685ef5251bf8e75890f938b39e91e0aeea7
Yushi Yang <yushiyang@Yushis-MacBook-Air.local>
2024-08-17 11:07:02 +0800

    Simplied the data ordering functions.

--- training/data_ordering.py
+++ training/data_ordering.py
@@ -6,8 +6,8 @@ import inspect
 
 # # Set up the threshold values for Easy/Medium/Hard questions
 ## MedQA - LLM difficulty thresholds (four models)
-# easy_threshold=0.66
-# med_threshold=0.80
+easy_threshold=0.66
+med_threshold=0.80
 
 category_col='cluster_label'
 difficulty_metric='llm_difficulty'
@@ -81,276 +81,41 @@ def interleaved_emh(df, blocked_learning_fn, category_col=category_col, difficul
 def original(df):
     return df
 
-def random_shuffle_1(df, seed=42):
+def random_shuffle(df, seed=42):
     return df.sample(frac=1, random_state=seed).reset_index(drop=True)
-
-def random_shuffle_2(df, seed=123):
-    return df.sample(frac=1, random_state=seed).reset_index(drop=True)
-
-def random_shuffle_3(df, seed=456):
-    return df.sample(frac=1, random_state=seed).reset_index(drop=True)
-
-def random_shuffle_4(df, seed=789):
-    return df.sample(frac=1, random_state=seed).reset_index(drop=True)
-
-
 
 def curriculum_strict(df, difficulty_metric=difficulty_metric):
     return df.sort_values(by=difficulty_metric, ascending=True).reset_index(drop=True)
 
-# def curriculum_strict_reverse(df, difficulty_metric=difficulty_metric):
-#     return df.sort_values(by=difficulty_metric, ascending=False).reset_index(drop=True)
-
-def curriculum_emh_1(df, difficulty_metric=difficulty_metric, seed=42):
+def curriculum_emh(df, difficulty_metric=difficulty_metric, seed=42):
     final_df = arrange_emh(df, difficulty_metric, seed)
     return final_df
-
-def curriculum_emh_2(df, difficulty_metric=difficulty_metric, seed=123):
-    final_df = arrange_emh(df, difficulty_metric, seed)
-    return final_df
-
-def curriculum_emh_3(df, difficulty_metric=difficulty_metric, seed=456):
-    final_df = arrange_emh(df, difficulty_metric, seed)
-    return final_df
-
-def curriculum_emh_4(df, difficulty_metric=difficulty_metric, seed=789):
-    final_df = arrange_emh(df, difficulty_metric, seed)
-    return final_df
-
-
 
 # The blocked learning ones have random category order and random data order in each category
-def blocked_1(df, category_col=category_col, seed=1):
+def blocked(df, category_col=category_col, seed=1):
     # Group by category, shuffle within each group
     return df.groupby(category_col, group_keys=False).apply(lambda x: x.sample(frac=1, random_state=seed)).reset_index(drop=True)
 
-def blocked_2(df, category_col=category_col):
-    # Count the number of items per category and get the category names in descending order of count
-    category_counts = df[category_col].value_counts().index.tolist()
-    
-    # Reorder the DataFrame based on the sorted category list with the most items first
-    ordered_df = pd.concat([df[df[category_col] == cat] for cat in category_counts], ignore_index=True)
-
-    return ordered_df
-
-def blocked_3(df, category_col=category_col, seed=42):
-    # Group by category, shuffle within each group
-    grouped_df = df.groupby(category_col, group_keys=False).apply(lambda x: x.sample(frac=1, random_state=seed)).reset_index(drop=True)
-    
-    # Extract unique categories, shuffle the list of categories
-    unique_categories = grouped_df[category_col].unique()
-    np.random.seed(seed) 
-    np.random.shuffle(unique_categories)
-
-    # Concatenate the DataFrames for each category in the order of the shuffled list
-    shuffled_df = pd.concat([grouped_df[grouped_df[category_col] == cat] for cat in unique_categories], ignore_index=True)
-    
-    return shuffled_df
-
-def blocked_4(df, category_col=category_col, seed=123):
-    # Group by category, shuffle within each group
-    grouped_df = df.groupby(category_col, group_keys=False).apply(lambda x: x.sample(frac=1, random_state=seed)).reset_index(drop=True)
-    
-    # Extract unique categories, shuffle the list of categories
-    unique_categories = grouped_df[category_col].unique()
-    np.random.seed(seed) 
-    np.random.shuffle(unique_categories)
-
-    # Concatenate the DataFrames for each category in the order of the shuffled list
-    shuffled_df = pd.concat([grouped_df[grouped_df[category_col] == cat] for cat in unique_categories], ignore_index=True)
-    
-    return shuffled_df
-
-
-def blocked_5(df, category_col=category_col, seed=456):
-    # Group by category, shuffle within each group
-    grouped_df = df.groupby(category_col, group_keys=False).apply(lambda x: x.sample(frac=1, random_state=seed)).reset_index(drop=True)
-    
-    # Extract unique categories, shuffle the list of categories
-    unique_categories = grouped_df[category_col].unique()
-    np.random.seed(seed) 
-    np.random.shuffle(unique_categories)
-
-    # Concatenate the DataFrames for each category in the order of the shuffled list
-    shuffled_df = pd.concat([grouped_df[grouped_df[category_col] == cat] for cat in unique_categories], ignore_index=True)
-    
-    return shuffled_df
-
-
-
-def blocked_curriculum_strict_1(df, category_col=category_col, difficulty_metric=difficulty_metric):
+def blocked_curriculum_strict(df, category_col=category_col, difficulty_metric=difficulty_metric):
     # Within each category, sort questions by ascending difficulty
     return df.groupby(category_col, group_keys=False).apply(lambda x: x.sort_values(by=difficulty_metric, ascending=True)).reset_index(drop=True)
 
-def blocked_curriculum_strict_2(df, category_col=category_col, difficulty_metric=difficulty_metric):
-    # Count the number of items per category and get the category names in descending order of count
-    category_counts = df[category_col].value_counts().index.tolist()
-    
-    # Reorder the DataFrame: sort within each category by difficulty, then concatenate based on category item count
-    ordered_df = pd.concat([df[df[category_col] == cat].sort_values(by=difficulty_metric, ascending=True) for cat in category_counts], ignore_index=True)
-
-    return ordered_df
-
-def blocked_curriculum_strict_3(df, category_col=category_col, difficulty_metric=difficulty_metric, seed=42):
-    # Group by category, shuffle within each group, then sort by 'difficulty_metric' in descending order
-    grouped_df = df.groupby(category_col, group_keys=False).apply(
-        lambda x: x.sort_values(by=difficulty_metric, ascending=True)
-    ).reset_index(drop=True)
-    
-    # Extract unique categories, shuffle the list of categories
-    unique_categories = grouped_df[category_col].unique()
-    np.random.seed(seed) 
-    np.random.shuffle(unique_categories)
-
-    # Concatenate the DataFrames for each category in the order of the shuffled list
-    shuffled_df = pd.concat([grouped_df[grouped_df[category_col] == cat] for cat in unique_categories], ignore_index=True)
-    
-    return shuffled_df
-
-def blocked_curriculum_strict_4(df, category_col=category_col, difficulty_metric=difficulty_metric, seed=123):
-    # Group by category, shuffle within each group, then sort by 'difficulty_metric' in descending order
-    grouped_df = df.groupby(category_col, group_keys=False).apply(
-        lambda x: x.sort_values(by=difficulty_metric, ascending=True)
-    ).reset_index(drop=True)
-    
-    # Extract unique categories, shuffle the list of categories
-    unique_categories = grouped_df[category_col].unique()
-    np.random.seed(seed) 
-    np.random.shuffle(unique_categories)
-
-    # Concatenate the DataFrames for each category in the order of the shuffled list
-    shuffled_df = pd.concat([grouped_df[grouped_df[category_col] == cat] for cat in unique_categories], ignore_index=True)
-    
-    return shuffled_df
-
-def blocked_curriculum_strict_5(df, category_col=category_col, difficulty_metric=difficulty_metric, seed=456):
-    # Group by category, shuffle within each group, then sort by 'difficulty_metric' in descending order
-    grouped_df = df.groupby(category_col, group_keys=False).apply(
-        lambda x: x.sort_values(by=difficulty_metric, ascending=True)
-    ).reset_index(drop=True)
-    
-    # Extract unique categories, shuffle the list of categories
-    unique_categories = grouped_df[category_col].unique()
-    np.random.seed(seed) 
-    np.random.shuffle(unique_categories)
-
-    # Concatenate the DataFrames for each category in the order of the shuffled list
-    shuffled_df = pd.concat([grouped_df[grouped_df[category_col] == cat] for cat in unique_categories], ignore_index=True)
-    
-    return shuffled_df
-
-
-
-def blocked_emh_1(df, category_col=category_col, difficulty_metric=difficulty_metric, seed=1):
+def blocked_emh(df, category_col=category_col, difficulty_metric=difficulty_metric, seed=1):
     # Use the arrange_emh function on each group to sort questions into EMH order
     final_df = df.groupby(category_col, group_keys=False).apply(lambda x: arrange_emh(x, difficulty_metric, seed)).reset_index(drop=True)
     return final_df
 
-def blocked_emh_2(df, category_col=category_col, difficulty_metric=difficulty_metric, seed=12):
-    # Get categories sorted by their frequency
-    category_counts = df[category_col].value_counts().index.tolist()
-    
-    # Apply arrange_emh for each category and concatenate the results
-    ordered_df = pd.concat([arrange_emh(df[df[category_col] == cat], difficulty_metric, seed) for cat in category_counts], ignore_index=True)
-    
-    return ordered_df
-
-def blocked_emh_3(df, category_col=category_col, difficulty_metric=difficulty_metric, seed=42):
-    # Shuffle within each category group first
-    grouped_df = df.groupby(category_col, group_keys=False).apply(lambda x: x.sample(frac=1, random_state=seed))
-
-    # Extract unique categories, shuffle the list of categories
-    np.random.seed(seed)
-    unique_categories = grouped_df[category_col].unique()
-    np.random.shuffle(unique_categories)
-
-    # Apply arrange_emh for each shuffled category and concatenate the results
-    shuffled_df = pd.concat([arrange_emh(grouped_df[grouped_df[category_col] == cat], difficulty_metric, seed) for cat in unique_categories], ignore_index=True)
-    
-    return shuffled_df
-
-def blocked_emh_4(df, category_col=category_col, difficulty_metric=difficulty_metric, seed=123):
-    # Shuffle within each category group first
-    grouped_df = df.groupby(category_col, group_keys=False).apply(lambda x: x.sample(frac=1, random_state=seed))
-
-    # Extract unique categories, shuffle the list of categories
-    np.random.seed(seed)
-    unique_categories = grouped_df[category_col].unique()
-    np.random.shuffle(unique_categories)
-
-    # Apply arrange_emh for each shuffled category and concatenate the results
-    shuffled_df = pd.concat([arrange_emh(grouped_df[grouped_df[category_col] == cat], difficulty_metric, seed) for cat in unique_categories], ignore_index=True)
-    
-    return shuffled_df
-
-def blocked_emh_5(df, category_col=category_col, difficulty_metric=difficulty_metric, seed=456):
-    # Shuffle within each category group first
-    grouped_df = df.groupby(category_col, group_keys=False).apply(lambda x: x.sample(frac=1, random_state=seed))
-
-    # Extract unique categories, shuffle the list of categories
-    np.random.seed(seed)
-    unique_categories = grouped_df[category_col].unique()
-    np.random.shuffle(unique_categories)
-
-    # Apply arrange_emh for each shuffled category and concatenate the results
-    shuffled_df = pd.concat([arrange_emh(grouped_df[grouped_df[category_col] == cat], difficulty_metric, seed) for cat in unique_categories], ignore_index=True)
-    
-    return shuffled_df
-
-
-
-
-def interleaved_1(df, category_col=category_col):
+def interleaved(df, category_col=category_col):
     # Call the general interleaved function with blocked_1 as the data ordering function
-    return interleaved(df, blocked_1, category_col)
+    return interleaved(df, blocked, category_col)
 
-def interleaved_2(df, category_col=category_col):
-    return interleaved(df, blocked_2, category_col)
-
-def interleaved_3(df, category_col=category_col):
-    return interleaved(df, blocked_3, category_col)
-
-def interleaved_4(df, category_col=category_col):
-    return interleaved(df, blocked_4, category_col)
-
-def interleaved_5(df, category_col=category_col):
-    return interleaved(df, blocked_5, category_col)
-
-
-
-def interleaved_curriculum_strict_1(df, category_col=category_col):
+def interleaved_curriculum_strict(df, category_col=category_col):
     # Call the general interleaved function with blocked_curriculum_strict_1 as the data ordering function
-    return interleaved(df, blocked_curriculum_strict_1, category_col)
+    return interleaved(df, blocked_curriculum_strict, category_col)
 
-def interleaved_curriculum_strict_2(df, category_col=category_col):
-    return interleaved(df, blocked_curriculum_strict_2, category_col)
-
-def interleaved_curriculum_strict_3(df, category_col=category_col):
-    return interleaved(df, blocked_curriculum_strict_3, category_col)
-
-def interleaved_curriculum_strict_4(df, category_col=category_col):
-    return interleaved(df, blocked_curriculum_strict_4, category_col)
-
-def interleaved_curriculum_strict_5(df, category_col=category_col):
-    return interleaved(df, blocked_curriculum_strict_5, category_col)
-
-
-
-def interleaved_emh_1(df, category_col=category_col, difficulty_metric=difficulty_metric):
+def interleaved_emh(df, category_col=category_col, difficulty_metric=difficulty_metric):
     # Call the interleaved_emh function with blocked_emh_1 as the data ordering function
-    return interleaved_emh(df, blocked_emh_1, category_col, difficulty_metric)
-
-def interleaved_emh_2(df, category_col=category_col, difficulty_metric=difficulty_metric):
-    return interleaved_emh(df, blocked_emh_2, category_col, difficulty_metric)
-
-def interleaved_emh_3(df, category_col=category_col, difficulty_metric=difficulty_metric):
-    return interleaved_emh(df, blocked_emh_3, category_col, difficulty_metric)
-
-def interleaved_emh_4(df, category_col=category_col, difficulty_metric=difficulty_metric):
-    return interleaved_emh(df, blocked_emh_4, category_col, difficulty_metric)
-
-def interleaved_emh_5(df, category_col=category_col, difficulty_metric=difficulty_metric):
-    return interleaved_emh(df, blocked_emh_5, category_col, difficulty_metric)
+    return interleaved_emh(df, blocked_emh, category_col, difficulty_metric)
 
 
 
@@ -401,15 +166,15 @@ if __name__ == "__main__":
 
     # List of data ordering functions
     data_ordering_functions = [
-        original, random_shuffle_1, random_shuffle_2, random_shuffle_3, random_shuffle_4,
-        curriculum_strict, curriculum_emh_1, curriculum_emh_2, curriculum_emh_3, curriculum_emh_4,
-        blocked_1, blocked_2, blocked_3, blocked_4, blocked_5,
-        blocked_curriculum_strict_1, blocked_curriculum_strict_2, blocked_curriculum_strict_3, blocked_curriculum_strict_4, blocked_curriculum_strict_5,
-        blocked_emh_1, blocked_emh_2, blocked_emh_3, blocked_emh_4, blocked_emh_5, 
-        interleaved_1, interleaved_2, interleaved_3, interleaved_4, interleaved_5,
-        interleaved_curriculum_strict_1, interleaved_curriculum_strict_2, interleaved_curriculum_strict_3, interleaved_curriculum_strict_4, interleaved_curriculum_strict_5,
-        interleaved_emh_1, interleaved_emh_2, interleaved_emh_3, interleaved_emh_4, interleaved_emh_5,
-        # curriculum_strict_reverse, curriculum_emh_reverse_1, curriculum_emh_reverse_2 # Reversed difficulty
+        original, random_shuffle, 
+        curriculum_strict, 
+        curriculum_emh, 
+        blocked, 
+        blocked_curriculum_strict,
+        blocked_emh,
+        interleaved, 
+        interleaved_curriculum_strict, 
+        interleaved_emh,
     ]
 
     # Call the main processing function
